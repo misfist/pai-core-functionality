126019d16fa3fbee6e524702e3ef1757315ea968
Pea <pea@misfist.com>
2017-10-21 20:47:24 -0400

    Added new custom fields

--- includes/custom-fields.php
+++ includes/custom-fields.php
@@ -39,7 +39,7 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   			'layout' => 'row',
   			'sub_fields' => array (
   				array (
-  					'key' => 'field_heading',
+  					'key' => 'field_hero_heading',
   					'label' => __( 'Heading', 'pai-core' ),
   					'name' => 'heading',
   					'type' => 'text',
@@ -58,7 +58,7 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   					'maxlength' => '',
   				),
   				array (
-  					'key' => 'field_text',
+  					'key' => 'field_hero_text',
   					'label' => __( 'Text', 'pai-core' ),
   					'name' => 'text',
   					'type' => 'wysiwyg',
@@ -77,7 +77,7 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   					'delay' => 0,
   				),
   				array (
-  					'key' => 'field_link',
+  					'key' => 'field_hero_link',
   					'label' => __( 'Link', 'pai' ),
   					'name' => 'link',
   					'type' => 'link',
@@ -93,6 +93,7 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   				),
   			),
   		),
+
   		array (
   			'key' => 'field_sections',
   			'label' => __( 'Sections', 'pai-core' ),
@@ -150,6 +151,68 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   					'append' => '',
   					'maxlength' => '',
   				),
+
+          /* Condition Field */
+          array (
+  					'key' => 'field_section_type',
+  					'label' => __( 'Section Type', 'pai-core' ),
+  					'name' => 'section_type',
+  					'type' => 'radio',
+  					'instructions' => '',
+  					'required' => 0,
+  					'conditional_logic' => 0,
+  					'wrapper' => array (
+  						'width' => '',
+  						'class' => '',
+  						'id' => '',
+  					),
+  					'choices' => array (
+  						'manual' => __( 'Manual', 'pai-core' ),
+  						'dynamic' => __( 'Dynamic Post List', 'pai-core' ),
+  					),
+            'allow_null' => 0,
+  					'other_choice' => 0,
+  					'save_other_choice' => 0,
+  					'default_value' => 'manual',
+  					'layout' => 'horizontal',
+  					'return_format' => 'value',
+  				),
+
+          /* Dynamic Post List Fields */
+          array (
+            'key' => 'field_display',
+            'label' => __( 'Display', 'pai-core' ),
+            'name' => 'display',
+            'type' => 'radio',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => array (
+              array (
+                array (
+                  'field' => 'field_section_type',
+                  'operator' => '==',
+                  'value' => 'dynamic',
+                ),
+              ),
+            ),
+            'wrapper' => array (
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'choices' => array (
+              'grid' => __( 'Grid', 'pai-core' ),
+              'slide' => __( 'Slider', 'pai-core' ),
+            ),
+            'allow_null' => 0,
+            'other_choice' => 0,
+            'save_other_choice' => 0,
+            'default_value' => 'grid',
+            'layout' => 'horizontal',
+            'return_format' => 'value',
+          ),
+
+          /* Manual Entry Fields */
   				array (
   					'key' => 'field_section_items',
   					'label' => __( 'Section Items', 'pai-core' ),
@@ -177,7 +240,15 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   									'type' => 'text',
   									'instructions' => '',
   									'required' => 0,
-  									'conditional_logic' => 0,
+                    'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'manual',
+  											),
+  										),
+  									),
   									'wrapper' => array (
   										'width' => '',
   										'class' => '',
@@ -193,10 +264,18 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   									'key' => 'field_text',
   									'label' => __( 'Text', 'pai-core' ),
   									'name' => 'text',
-  									'type' => 'textarea',
+  									'type' => 'wysiwyg',
   									'instructions' => '',
   									'required' => 0,
-  									'conditional_logic' => 0,
+                    'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'manual',
+  											),
+  										),
+  									),
   									'wrapper' => array (
   										'width' => '',
   										'class' => '',
@@ -208,6 +287,33 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   									'rows' => '',
   									'new_lines' => '',
   								),
+                  array (
+  									'key' => 'field_shortcode',
+  									'label' => __( 'Shortcode', 'pai-core' ),
+  									'name' => 'shortcode',
+  									'type' => 'text',
+  									'instructions' => __( 'Use this field to display a shortcode', 'pai-core' ),
+  									'required' => 0,
+                    'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'manual',
+  											),
+  										),
+  									),
+  									'wrapper' => array (
+  										'width' => '',
+  										'class' => '',
+  										'id' => '',
+  									),
+  									'default_value' => '',
+  									'placeholder' => '',
+  									'prepend' => '',
+  									'append' => '',
+  									'maxlength' => '',
+  								),
   								array (
   									'key' => 'field_link',
   									'label' => __( 'Link', 'pai-core' ),
@@ -215,7 +321,15 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   									'type' => 'link',
   									'instructions' => '',
   									'required' => 0,
-  									'conditional_logic' => 0,
+                    'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'manual',
+  											),
+  										),
+  									),
   									'wrapper' => array (
   										'width' => '',
   										'class' => '',
@@ -230,7 +344,15 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   									'type' => 'image',
   									'instructions' => '',
   									'required' => 0,
-  									'conditional_logic' => 0,
+                    'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'manual',
+  											),
+  										),
+  									),
   									'wrapper' => array (
   										'width' => '',
   										'class' => '',
@@ -247,6 +369,187 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   									'max_size' => '',
   									'mime_types' => '',
   								),
+
+                  array (
+  									'key' => 'field_post_type',
+  									'label' => __( 'Post Type', 'pai-core' ),
+  									'name' => 'post_type',
+  									'type' => 'select',
+  									'instructions' => '',
+  									'required' => 0,
+  									'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'dynamic',
+  											),
+  										),
+  									),
+                    'wrapper' => array (
+                      'width' => '',
+                      'class' => '',
+                      'id' => '',
+                    ),
+                    'choices' => array (
+                    ),
+                    'default_value' => array (
+                    ),
+                    'allow_null' => 0,
+                    'multiple' => 0,
+                    'ui' => 0,
+                    'ajax' => 0,
+                    'return_format' => 'value',
+                    'placeholder' => '',
+  								),
+
+  								array (
+  									'key' => 'field_posts_per_page',
+  									'label' => __( 'Number of Posts', 'pai-core' ),
+  									'name' => 'posts_per_page',
+  									'type' => 'number',
+  									'instructions' => '',
+  									'required' => 0,
+  									'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'dynamic',
+  											),
+  										),
+  									),
+  									'wrapper' => array (
+  										'width' => '',
+  										'class' => '',
+  										'id' => '',
+  									),
+  									'default_value' => 3,
+  									'placeholder' => '',
+  									'prepend' => '',
+  									'append' => '',
+  									'min' => '',
+  									'max' => '',
+  									'step' => '',
+  								),
+  								array (
+  									'key' => 'field_post_tag',
+  									'label' => __( 'Tag', 'pai-core' ),
+  									'name' => 'post_tag',
+  									'type' => 'taxonomy',
+  									'instructions' => '',
+  									'required' => 0,
+  									'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'dynamic',
+  											),
+  										),
+  									),
+  									'wrapper' => array (
+  										'width' => '',
+  										'class' => '',
+  										'id' => '',
+  									),
+  									'taxonomy' => 'post_tag',
+  									'field_type' => 'select',
+  									'allow_null' => 1,
+  									'add_term' => 0,
+  									'save_terms' => 0,
+  									'load_terms' => 0,
+  									'return_format' => 'id',
+  									'multiple' => 0,
+  								),
+  								array (
+  									'key' => 'field_category',
+  									'label' => __( 'Category', 'pai-core' ),
+  									'name' => 'category',
+  									'type' => 'taxonomy',
+  									'instructions' => '',
+  									'required' => 0,
+  									'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'dynamic',
+  											),
+  										),
+  									),
+  									'wrapper' => array (
+  										'width' => '',
+  										'class' => '',
+  										'id' => '',
+  									),
+  									'taxonomy' => 'category',
+  									'field_type' => 'select',
+  									'allow_null' => 1,
+  									'add_term' => 0,
+  									'save_terms' => 0,
+  									'load_terms' => 0,
+  									'return_format' => 'id',
+  									'multiple' => 0,
+  								),
+  								array (
+  									'key' => 'field_series',
+  									'label' => __( 'Series', 'pai-core' ),
+  									'name' => 'series',
+  									'type' => 'taxonomy',
+  									'instructions' => '',
+  									'required' => 0,
+  									'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'dynamic',
+  											),
+  										),
+  									),
+  									'wrapper' => array (
+  										'width' => '',
+  										'class' => '',
+  										'id' => '',
+  									),
+  									'taxonomy' => 'series',
+  									'field_type' => 'select',
+  									'allow_null' => 1,
+  									'add_term' => 0,
+  									'save_terms' => 0,
+  									'load_terms' => 0,
+  									'return_format' => 'id',
+  									'multiple' => 0,
+  								),
+  								array (
+  									'key' => 'field_display_featured_image',
+  									'label' => __( 'Display Featured Image?', 'pai-core' ),
+  									'name' => 'display_featured_image',
+  									'type' => 'true_false',
+  									'instructions' => '',
+  									'required' => 0,
+  									'conditional_logic' => array (
+  										array (
+  											array (
+  												'field' => 'field_section_type',
+  												'operator' => '==',
+  												'value' => 'dynamic',
+  											),
+  										),
+  									),
+  									'wrapper' => array (
+  										'width' => '',
+  										'class' => '',
+  										'id' => '',
+  									),
+  									'message' => '',
+  									'default_value' => 1,
+  									'ui' => 0,
+  									'ui_on_text' => '',
+  									'ui_off_text' => '',
+  								),
+
   							),
   							'min' => '',
   							'max' => '',
@@ -278,4 +581,94 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
   	'description' => '',
   ));
 
+  /* Press Mention Fields */
+  acf_add_local_field_group( array(
+  	'key' => 'group_press_mention',
+  	'title' => __( 'Details', 'pai-core' ),
+  	'fields' => array(
+  		array(
+  			'key' => 'field_publication',
+  			'label' => __( 'Publication', 'pai-core' ),
+  			'name' => 'publication',
+  			'type' => 'text',
+  			'instructions' => '',
+  			'required' => 0,
+  			'conditional_logic' => 0,
+  			'wrapper' => array (
+  				'width' => '',
+  				'class' => '',
+  				'id' => '',
+  			),
+  			'default_value' => '',
+  			'placeholder' => '',
+  			'prepend' => '',
+  			'append' => '',
+  			'maxlength' => '',
+  		),
+  		array (
+  			'key' => 'field_source_url',
+  			'label' => __( 'Source URL', 'pai-core' ),
+  			'name' => 'source_url',
+  			'type' => 'url',
+  			'instructions' => '',
+  			'required' => 0,
+  			'conditional_logic' => 0,
+  			'wrapper' => array (
+  				'width' => '',
+  				'class' => '',
+  				'id' => '',
+  			),
+  			'default_value' => '',
+  			'placeholder' => __( 'https://example.com', 'pai-core' ),
+  		),
+  	),
+  	'location' => array (
+  		array (
+  			array (
+  				'param' => 'post_category',
+  				'operator' => '==',
+  				'value' => 'category:press-mention',
+  			),
+  		),
+  	),
+  	'menu_order' => 0,
+  	'position' => 'normal',
+  	'style' => 'default',
+  	'label_placement' => 'top',
+  	'instruction_placement' => 'label',
+  	'hide_on_screen' => '',
+  	'active' => 1,
+  	'description' => '',
+  ));
+
 }
+
+/**
+ * Prepopulate Field
+ * Populate `post_type` field with post types
+ *
+ * @since 0.1.0
+ *
+ * @uses get_post_types
+ *
+ * @param obj $field
+ * @return obj field
+ */
+function pai_core_load_post_types( $field ) {
+
+  $args = array(
+    'public'    => true,
+  );
+  $output = 'object';
+
+  $post_types = get_post_types( $args, $output );
+
+  $field['choices'] = array();
+
+  foreach( $post_types as $post_type ) {
+    $field['choices'][$post_type->name] = $post_type->label;
+  }
+
+  return $field;
+}
+add_filter('acf/load_field/key=field_post_type', 'pai_core_load_post_types');
